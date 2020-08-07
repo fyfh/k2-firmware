881adf59fe307aced380ebb934374556ef0922e9
fyfh <13757260730@sina.cn>
2020-08-07 20:51:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,7 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/7872/8192/' ./target/linux/ramips/image/mt7620.mk
+sed -i 's/7872/16384/' ./target/linux/ramips/image/mt7620.mk
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
