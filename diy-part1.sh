9444524e342bab9f90c020e99c31bdf56f4837a5
fyfh <13757260730@sina.cn>
2020-09-02 23:33:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,6 +11,5 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i 's/7872/16384/' ./target/linux/ramips/image/mt7620.mk
-echo 'src-git mtk https://github.com/Nossiac/mtk-openwrt-feeds' >> feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
