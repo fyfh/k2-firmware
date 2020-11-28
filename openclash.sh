b1d186485c33578bd295d10a5240fac254244580
fyfh <13757260730@sina.cn>
2020-11-28 21:29:10 +0800

    Update openclash.sh

--- openclash.sh
+++ openclash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-cd openwrt && git clone https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/vernesong/OpenClash package/OpenClash
 ./scripts/feeds update luci-app-openclash
 ./scripts/feeds install luci-app-openclash
